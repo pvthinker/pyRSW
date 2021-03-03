2d31007b7364f0eaf29862dacf8aa6dcb8b1d640
pvthinker <roullet@univ-brest.fr>
2021-03-03 15:12:26 +0100

    Add the possibility to store the MPI case in a single history
    this is set as the default

--- core/ncio.py
+++ core/ncio.py
@@ -9,6 +9,14 @@ class Ncio(object):
     def __init__(self, param, grid, state):
         self.param = param
         self.grid = grid
+
+        self.halo_included = param.halo_included
+        self.singlefile = param.singlefile
+
+        self.nprocs = np.prod(param.procs)
+        if self.nprocs > 1:
+            from mpi4py import MPI
+            self.MPI = MPI
         # topography is not yet defined when Ncio is called
         # self.create_history_file(state, grid)
         self.kt = 0
@@ -21,7 +29,10 @@ class Ncio(object):
             pass
         else:
             raise ValueError(f"{param.filemode} is not yet implemented")
-        hisname = f"history_{param.myrank:02}.nc"
+        if self.singlefile:
+            hisname = f"history.nc"
+        else:
+            hisname = f"history_{param.myrank:02}.nc"
         diagname = f"diags.nc"
         self.hist_path = os.path.join(out_dir, hisname)
         self.diag_path = os.path.join(out_dir, diagname)
@@ -32,8 +43,6 @@ class Ncio(object):
             if not os.path.isdir(self.output_directory):
                 os.makedirs(self.output_directory)
         self.dtype = np.dtype(self.param.hisdtype)
-
-        self.halo_included = param.halo_included
 
     def backup_paramfile(self):
         self.paramfile = f"{self.output_directory}/param.pkl"
@@ -62,88 +71,91 @@ class Ncio(object):
         # for nickname in state.variables:
         #     if state.variables[nickname]["constant"]:
         #         self.gridvar += [nickname]
+        if not self.singlefile or (self.param.myrank == 0):
+            with Dataset(self.hist_path, "w", format='NETCDF4') as nc:
+                # Store the experiment parameters
+                attrs = {key: self.param[key] for key in self.param.toc}
+                # replace bool with int and transform dict into str
+                for key in attrs:
+                    if isinstance(attrs[key], bool):
+                        attrs[key] *= 1
+                    if isinstance(attrs[key], dict):
+                        attrs[key] = str(attrs[key])
+                    # force 32 bits integers (default is 64 in Python)
+                    if isinstance(attrs[key], int):
+                        attrs[key] = np.int32(attrs[key])
+                    # force 32 bits in list of integers (namely, loc and procs)
+                    if isinstance(attrs[key], list):
+                        for k, elem in enumerate(attrs[key]):
+                            if isinstance(elem, int):
+                                attrs[key][k] = np.int32(elem)
 
-        with Dataset(self.hist_path, "w", format='NETCDF4') as nc:
-            # Store the experiment parameters
-            attrs = {key: self.param[key] for key in self.param.toc}
-            # replace bool with int and transform dict into str
-            for key in attrs:
-                if isinstance(attrs[key], bool):
-                    attrs[key] *= 1
-                if isinstance(attrs[key], dict):
-                    attrs[key] = str(attrs[key])
-                # force 32 bits integers (default is 64 in Python)
-                if isinstance(attrs[key], int):
-                    attrs[key] = np.int32(attrs[key])
-                # force 32 bits in list of integers (namely, loc and procs)
-                if isinstance(attrs[key], list):
-                    for k, elem in enumerate(attrs[key]):
-                        if isinstance(elem, int):
-                            attrs[key][k] = np.int32(elem)
+                nc.setncatts(attrs)
 
-            nc.setncatts(attrs)
-
-            self.ndim = self.grid.xc.ndim
-            if self.ndim == 1:
-                nx = len(self.grid.xc)
-                ny = len(self.grid.yc)
-            elif self.ndim == 2:
-                if self.halo_included:
-                    ny, nx = self.grid.xc.shape
+                self.ndim = self.grid.xc.ndim
+                if self.ndim == 1:
+                    nx = len(self.grid.xc)
+                    ny = len(self.grid.yc)
+                elif self.ndim == 2:
+                    if self.halo_included:
+                        ny, nx = self.grid.xc.shape
+                    else:
+                        ny, nx = self.grid.ny, self.grid.nx
+                        if self.singlefile:
+                            nx *= self.param.npx
+                            ny *= self.param.npy
                 else:
-                    ny, nx = self.grid.ny, self.grid.nx
-            else:
-                raise ValueError
+                    raise ValueError
 
-            # Create the dimensions
-            nc.createDimension("t", None)  # unlimited size
-            nc.createDimension("tdiag", None)  # unlimited size
-            nc.createDimension("xc", nx)
-            nc.createDimension("yc", ny)
-            nc.createDimension("xe", nx+1)
-            nc.createDimension("ye", ny+1)
-            nc.createDimension("z", self.param.nz)
+                # Create the dimensions
+                nc.createDimension("t", None)  # unlimited size
+                nc.createDimension("tdiag", None)  # unlimited size
+                nc.createDimension("xc", nx)
+                nc.createDimension("yc", ny)
+                nc.createDimension("xe", nx+1)
+                nc.createDimension("ye", ny+1)
+                nc.createDimension("z", self.param.nz)
 
-            # Create the variables with one dimension
-            v = nc.createVariable("n", int, ("t",))
-            v.standard_name = "integration step in the model run"
+                # Create the variables with one dimension
+                v = nc.createVariable("n", int, ("t",))
+                v.standard_name = "integration step in the model run"
 
-            v = nc.createVariable("t", self.dtype, ("t",))
-            v.standard_name = "time in the model run"
-            v.units = "s"
+                v = nc.createVariable("t", self.dtype, ("t",))
+                v.standard_name = "time in the model run"
+                v.units = "s"
 
-            if self.ndim == 1:
-                v = nc.createVariable("xc", self.dtype, ("xc",))
-                v.standard_name = "x coordinate at cell centers"
-                v.units = "m"
+                if self.ndim == 1:
+                    v = nc.createVariable("xc", self.dtype, ("xc",))
+                    v.standard_name = "x coordinate at cell centers"
+                    v.units = "m"
 
-                v = nc.createVariable("xe", self.dtype, ("xe",))
-                v.standard_name = "x coordinate at cell edges"
-                v.units = "m"
+                    v = nc.createVariable("xe", self.dtype, ("xe",))
+                    v.standard_name = "x coordinate at cell edges"
+                    v.units = "m"
 
-                v = nc.createVariable("yc", self.dtype, ("yc",))
-                v.standard_name = "y  coordinate at cell centers"
-                v.units = "m"
+                    v = nc.createVariable("yc", self.dtype, ("yc",))
+                    v.standard_name = "y  coordinate at cell centers"
+                    v.units = "m"
 
-                v = nc.createVariable("ye", self.dtype, ("ye",))
-                v.standard_name = "y  coordinate at cell edges"
-                v.units = "m"
-            else:
-                v = nc.createVariable("xc", self.dtype, ("yc", "xc"))
-                v.standard_name = "x coordinate at cell centers"
-                v.units = "m"
+                    v = nc.createVariable("ye", self.dtype, ("ye",))
+                    v.standard_name = "y  coordinate at cell edges"
+                    v.units = "m"
+                else:
+                    v = nc.createVariable("xc", self.dtype, ("yc", "xc"))
+                    v.standard_name = "x coordinate at cell centers"
+                    v.units = "m"
 
-                v = nc.createVariable("xe", self.dtype, ("ye", "xe"))
-                v.standard_name = "x coordinate at cell edges"
-                v.units = "m"
+                    v = nc.createVariable("xe", self.dtype, ("ye", "xe"))
+                    v.standard_name = "x coordinate at cell edges"
+                    v.units = "m"
 
-                v = nc.createVariable("yc", self.dtype, ("yc", "xc"))
-                v.standard_name = "y  coordinate at cell centers"
-                v.units = "m"
+                    v = nc.createVariable("yc", self.dtype, ("yc", "xc"))
+                    v.standard_name = "y  coordinate at cell centers"
+                    v.units = "m"
 
-                v = nc.createVariable("ye", self.dtype, ("ye", "xe"))
-                v.standard_name = "y  coordinate at cell edges"
-                v.units = "m"
+                    v = nc.createVariable("ye", self.dtype, ("ye", "xe"))
+                    v.standard_name = "y  coordinate at cell edges"
+                    v.units = "m"
 
         self.hisvar = []
         for nickname in self.param.var_to_save+self.gridvar:
@@ -175,52 +187,110 @@ class Ncio(object):
                 self.createvar(nickname+"y", dims,
                                var["name"]+" y-component", var["unit"])
 
-        if self.halo_included:
-            with Dataset(self.hist_path, "r+") as nc:
-                nc.variables["xc"][:] = self.grid.xc
-                nc.variables["yc"][:] = self.grid.yc
-                nc.variables["xe"][:] = self.grid.xe
-                nc.variables["ye"][:] = self.grid.ye
-        else:
-            with Dataset(self.hist_path, "r+") as nc:
-                j0, j1, i0, i1 = self.grid.arrays.hb.domainindices
-                nc.variables["xc"][:] = self.grid.xc[j0:j1, i0:i1]
-                nc.variables["yc"][:] = self.grid.yc[j0:j1, i0:i1]
-                j0, j1, i0, i1 = self.grid.arrays.f.domainindices
-                nc.variables["xe"][:] = self.grid.xe[j0:j1, i0:i1]
-                nc.variables["ye"][:] = self.grid.ye[j0:j1, i0:i1]
+        # if self.nprocs > 1:
+        #     self.MPI.COMM_WORLD.Barrier()
+        #     print("ok", self.param.loc, flush=True)
 
-        # store grid arrays (f, hb, msk)
-        with Dataset(self.hist_path, "r+") as nc:
-            for nickname in self.gridvar:
-                var = self.grid.arrays.get(nickname)
-                data = var.getproperunits(self.grid)
+        if self.singlefile:
+            I0 = self.param.loc[2]*self.param.nx
+            J0 = self.param.loc[1]*self.param.ny
+        else:
+            I0 = 0
+            J0 = 0
+        nxc = self.param.nx
+        nxe = nxc+1
+        nyc = self.param.ny
+        nye = nyc+1
+        self.indices = (I0, J0, nxc, nyc, nxe, nye)
+
+        for rank in range(self.nprocs):
+            if rank == self.param.myrank:
                 if self.halo_included:
-                    nc.variables[nickname][:] = data
+                    with Dataset(self.hist_path, "r+") as nc:
+                        nc.variables["xc"][:] = self.grid.xc
+                        nc.variables["yc"][:] = self.grid.yc
+                        nc.variables["xe"][:] = self.grid.xe
+                        nc.variables["ye"][:] = self.grid.ye
                 else:
-                    j0, j1, i0, i1 = var.domainindices
-                    nc.variables[nickname][self.kt] = data[j0:j1, i0:i1]
+                    with Dataset(self.hist_path, "r+") as nc:
+                        j0, j1, i0, i1 = self.grid.arrays.hb.domainindices
+                        nc.variables["xc"][J0:J0+nyc, I0:I0 +
+                                           nxc] = self.grid.xc[j0:j1, i0:i1]
+                        nc.variables["yc"][J0:J0+nyc, I0:I0 +
+                                           nxc] = self.grid.yc[j0:j1, i0:i1]
+                        j0, j1, i0, i1 = self.grid.arrays.f.domainindices
+                        nc.variables["xe"][J0:J0+nye, I0:I0 +
+                                           nxe] = self.grid.xe[j0:j1, i0:i1]
+                        nc.variables["ye"][J0:J0+nye, I0:I0 +
+                                           nxe] = self.grid.ye[j0:j1, i0:i1]
+
+                # store grid arrays (f, hb, msk)
+                with Dataset(self.hist_path, "r+") as nc:
+                    for nickname in self.gridvar:
+                        var = self.grid.arrays.get(nickname)
+                        data = var.getproperunits(self.grid)
+                        if self.halo_included:
+                            nc.variables[nickname][:] = data
+                        else:
+                            j0, j1, i0, i1 = var.domainindices
+                            if "xy" in var.stagg:
+                                nc.variables[nickname][J0:J0+nye,
+                                                       I0:I0+nxe] = data[j0:j1, i0:i1]
+                            elif var.stagg == "":
+                                # print(nc.variables[nickname].shape, J0, I0, nyc, nxc,j0,j1,i0,i1,flush=True)
+                                nc.variables[nickname][J0:J0+nyc,
+                                                       I0:I0+nxc] = data[j0:j1, i0:i1]
+
+            if self.nprocs > 1:
+                self.MPI.COMM_WORLD.Barrier()
+        # self.MPI.COMM_WORLD.Barrier()
+        # print("ok",flush=True)
 
     def createvar(self, nickname, dims, name, unit):
         # print(f"create variable {nickname}")
-        with Dataset(self.hist_path, "r+", format='NETCDF4') as nc:
-            v = nc.createVariable(nickname, self.dtype, tuple(dims))
-            v.standard_name = name
-            v.units = unit
+        if not self.singlefile or (self.param.myrank == 0):
+            with Dataset(self.hist_path, "r+", format='NETCDF4') as nc:
+                v = nc.createVariable(nickname, self.dtype, tuple(dims))
+                v.standard_name = name
+                v.units = unit
         if nickname not in self.gridvar:
             self.hisvar += [nickname]
 
     def dohis(self, state, time):
-        with Dataset(self.hist_path, "r+") as nc:
-            nc.variables['n'][self.kt] = self.kt
-            nc.variables['t'][self.kt] = time
-            for nickname in self.hisvar:
-                data = state.get(nickname).getproperunits(self.grid)
-                if self.halo_included:
-                    nc.variables[nickname][self.kt] = data
-                else:
-                    k0, k1, j0, j1, i0, i1 = state.get(nickname).domainindices
-                    nc.variables[nickname][self.kt] = data[:, j0:j1, i0:i1]
+        I0, J0, nxc, nyc, nxe, nye = self.indices
+        if not self.singlefile or (self.param.myrank == 0):
+            with Dataset(self.hist_path, "r+") as nc:
+                nc.variables['n'][self.kt] = self.kt
+                nc.variables['t'][self.kt] = time
+
+        for rank in range(self.nprocs):
+            # if self.nprocs>1:
+            #     self.MPI.COMM_WORLD.Barrier()
+            if rank == self.param.myrank:
+                with Dataset(self.hist_path, "r+") as nc:
+                    for nickname in self.hisvar:
+                        var = state.get(nickname)
+                        data = var.getproperunits(self.grid)
+                        if self.halo_included:
+                            nc.variables[nickname][self.kt] = data
+                        else:
+                            k0, k1, j0, j1, i0, i1 = state.get(
+                                nickname).domainindices
+                            if "xy" in var.stagg:
+                                nc.variables[nickname][self.kt, :, J0:J0 +
+                                                       nye, I0:I0+nxe] = data[:, j0:j1, i0:i1]
+                            elif "x" in var.stagg:
+                                nc.variables[nickname][self.kt, :, J0:J0 +
+                                                       nyc, I0:I0+nxe] = data[:, j0:j1, i0:i1]
+                            elif "y" in var.stagg:
+                                nc.variables[nickname][self.kt, :, J0:J0 +
+                                                       nye, I0:I0+nxc] = data[:, j0:j1, i0:i1]
+                            else:
+                                nc.variables[nickname][self.kt, :, J0:J0 +
+                                                       nyc, I0:I0+nxc] = data[:, j0:j1, i0:i1]
+
+            if self.nprocs > 1:
+                self.MPI.COMM_WORLD.Barrier()
         self.kt += 1
 
     def dodiags(self, diags, time, kt):
